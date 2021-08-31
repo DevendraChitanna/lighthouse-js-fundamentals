6890d8d25aaf7511a59b21aaae329fb5fa54aa63
DevendraChitanna <devendrachitanna@gmail.com>
2021-08-31 12:39:13 -0400

    Used a function to print Hello and a then a name

---
+++ hello.js
@@ -0,0 +1,16 @@
+/*
+const sayHello  = function () {
+  console.log("Hello, world");
+}
+ */
+
+
+ const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Devendra");
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
